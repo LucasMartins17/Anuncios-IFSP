3693e4ef92efd008a04e40058ead79ba612be418
GABRIEL PAULINO RODRIGUES TELES <gabrielteles384@gmail.com>
2024-06-17 16:29:32 -0300

    Unificação dos BDs

--- ParteGabrielP/geral.sql
+++ ParteGabrielP/geral.sql
@@ -30,3 +30,33 @@ CREATE TABLE usuario (
     dataInscricao VARCHAR(255) NOT NULL,
     FOREIGN KEY (Id_func) REFERENCES funcao(Id)
 );
+
+
+
+----------------------------------- Parte do Grupo dos Anuncios -----------------------------------
+
+-- Criação da tabela Form
+CREATE TABLE Form (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Titulo VARCHAR(255) NOT NULL UNIQUE,
+    Descricao VARCHAR(255) NOT NULL,
+    DataInicio DATETIME NOT NULL,
+    HrIni DATETIME NOT NULL,
+    DataFinal DATETIME NOT NULL,
+    HoraFinal DATETIME NOT NULL,
+    Tipo VARCHAR(255) NOT NULL,
+    pubAlv VARCHAR(255) NOT NULL
+);
+
+-- Criação da tabela Artes
+CREATE TABLE Artes (
+    Id INT,
+    Titulo VARCHAR(255),
+    caminhoImg VARCHAR(255),
+    PRIMARY KEY (Id),
+    FOREIGN KEY (Id) REFERENCES Form(Id),
+    FOREIGN KEY (Titulo) REFERENCES Form(Titulo)
+);
+
+
+----------------------------------- Fim da parte do Grupo dos Anuncios -----------------------------------
